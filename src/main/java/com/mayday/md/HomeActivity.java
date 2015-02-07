10d6e1698fe2f7242fc39ea8c2d1c4dc7cd5ff46
Jack Shultz <jackygrahamez@gmail.com>
2015-02-07 15:35:53 -0500

    Revert "updated with bad dialog code"
    This reverts commit 5b7bf712ed7006cd3db0e879203743d8cdb3f2aa.

--- src/main/java/com/mayday/md/HomeActivity.java
+++ src/main/java/com/mayday/md/HomeActivity.java
@@ -2,15 +2,10 @@ package com.mayday.md;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
-import android.app.ListActivity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 import com.mayday.md.common.AppConstants;
 import com.mayday.md.common.ApplicationSettings;
@@ -53,43 +48,6 @@ public class HomeActivity extends Activity {
         setContentView(R.layout.welcome_screen);
 
         //deleteShortCut();
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, NAMES);
-
-        ListView mListView = (ListView) findViewById(R.id.demo_list);
-
-        mListView.setAdapter(adapter);
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                if (position == MAYDAY_CUP) {
-                    if (mHelloCupDialog == null) {
-                        mHelloCupDialog = new GearFitDialog(getApplicationContext());
-                    } else {
-                        mHelloCupDialog.finish();
-                        mHelloCupDialog = null;
-                    }
-                }
-            }
-        });
-
-        /*
-        mListView.setOnItemClickListener(new Adapter.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // S Pen SDK Demo programs
-                if (position == MAYDAY_CUP) {
-                    if (mHelloCupDialog == null) {
-                        mHelloCupDialog = new GearFitDialog(getApplicationContext());
-                    } else {
-                        mHelloCupDialog.finish();
-                        mHelloCupDialog = null;
-                    }
-                }
-            }
-        });
-        */
 
         latestVersion = -1;
         lastUpdatedVersion = ApplicationSettings.getLastUpdatedVersion(HomeActivity.this);
