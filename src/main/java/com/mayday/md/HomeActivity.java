a7e5f36b2bc0a8e35430e12f12136656708dd470
Jack Shultz <jackygrahamez@gmail.com>
2015-02-11 09:49:29 -0500

    moved gear fit trigger to a new class

--- src/main/java/com/mayday/md/HomeActivity.java
+++ src/main/java/com/mayday/md/HomeActivity.java
@@ -2,6 +2,7 @@ package com.mayday.md;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
@@ -17,6 +18,7 @@ import com.mayday.md.common.AppConstants;
 import com.mayday.md.common.ApplicationSettings;
 import com.mayday.md.data.PBDatabase;
 import com.mayday.md.model.Page;
+import com.mayday.md.trigger.GearFitTrigger;
 import com.mayday.md.trigger.HardwareTriggerReceiver;
 import com.mayday.md.trigger.HardwareTriggerService;
 import com.mayday.md.GearFitDialog;
@@ -53,41 +55,15 @@ public class HomeActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_screen);
+        Context context = getApplicationContext();
         SharedPreferences mPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         HardwareTriggerReceiver hardwareTriggerReceiver = new HardwareTriggerReceiver();
+        GearFitTrigger gearFitTrigger = new GearFitTrigger();
 
         Bundle bundle=getIntent().getExtras();
         boolean startedByCUP=false;
-        if(bundle!=null) {
-            if (!hardwareTriggerReceiver.isCallActive(getApplicationContext())) {
-                int c = mPref.getInt("numRun", 0);
-                int TIME_INTERVAL = 10000;
-                int TOTAL_CLICKS = 5;
-                long delta = 0;
-                Long eventTime = System.currentTimeMillis();
-                mPref.edit().putLong("eventTime", eventTime).commit();
-                Long firstEventTime = mPref.getLong("firstEventTime", 0);
-                if (firstEventTime == 0) {
-                    firstEventTime = eventTime;
-                    mPref.edit().putLong("firstEventTime", firstEventTime).commit();
-                }
-                delta = eventTime - firstEventTime;
-                Log.e(">>>>>>", "START_BY_CUP delta "+delta);
-                if (delta < TIME_INTERVAL) {
-                    c++;
-                    mPref.edit().putInt("numRun",c).commit();
-                    Log.e(">>>>>>", "START_BY_CUP "+c);
-                    if (c >=TOTAL_CLICKS) {
-                        hardwareTriggerReceiver.onActivation(getApplicationContext());
-                        mPref.edit().putInt("numRun", 0).apply();
-                        mPref.edit().putLong("firstEventTime", 0).apply();
-                    }
-                } else {
-                    mPref.edit().putInt("numRun", 0).apply();
-                    mPref.edit().putLong("firstEventTime", 0).apply();
-                }
-            }
-        }
+
+        gearFitTrigger.GearFitTrigger(bundle, context, mPref);
 
         //deleteShortCut();
 
