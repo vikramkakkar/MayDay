55b21124ab8602bb9248155e46c97739c7cb6f1a
Jack Shultz <jackygrahamez@gmail.com>
2015-02-12 12:03:11 -0500

    added java files

--- mobile/src/main/java/io/mayday/md/MainActivity.java
+++ mobile/src/main/java/io/mayday/md/MainActivity.java
@@ -1,7 +1,247 @@
 package io.mayday.md;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import io.mayday.md.alert.PanicAlert;
+import io.mayday.md.common.AppConstants;
+import io.mayday.md.common.ApplicationSettings;
+import io.mayday.md.data.PBDatabase;
+import io.mayday.md.fragment.LanguageSettingsFragment;
+import io.mayday.md.fragment.MainSetupAlertFragment;
+import io.mayday.md.fragment.SetupCodeFragment;
+import io.mayday.md.fragment.SetupContactsFragment;
+import io.mayday.md.fragment.SetupMessageFragment;
+import io.mayday.md.fragment.SimpleFragment;
+import io.mayday.md.fragment.WarningFragment;
+import io.mayday.md.model.Page;
+import io.mayday.md.trigger.HardwareTriggerService;
+
+
 /**
- * Created by jshultz on 2/12/15.
+ * Created by aoe on 2/15/14.
  */
-public class MainActivity {
+public class MainActivity extends BaseFragmentActivity {
+
+    TextView tvToastMessage;
+
+    Page currentPage;
+    String pageId;
+    String selectedLang;
+
+    Boolean flagRiseFromPause = false;
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.root_layout);
+        
+        tvToastMessage = (TextView) findViewById(R.id.tv_toast);
+
+        try {
+            pageId = getIntent().getExtras().getString("page_id");
+        } catch (Exception e) {
+            pageId = "home-not-configured";
+            e.printStackTrace();
+        }
+
+        selectedLang = ApplicationSettings.getSelectedLanguage(this);
+
+        Log.e("MainActivity.onCreate", "pageId = " + pageId);
+
+        if(pageId.equals("home-not-configured")){
+            Log.e("??????????????", "Restarting the Wizard");
+
+            if((ApplicationSettings.isAlertActive(this))){
+                new PanicAlert(this).deActivate();
+            }
+
+            ApplicationSettings.setWizardState(MainActivity.this, AppConstants.WIZARD_FLAG_HOME_NOT_CONFIGURED);
+            //changeAppIcontoPB();
+
+            // We're restarting the wizard so we deactivate the HardwareTriggerService
+            stopService(new Intent(this, HardwareTriggerService.class));
+
+
+            Intent i = new Intent(MainActivity.this, WizardActivity.class);
+            i.putExtra("page_id", pageId);
+            startActivity(i);
+
+            callFinishActivityReceiver();
+
+            Intent broadcastIntent = new Intent();
+            broadcastIntent.setAction("io.mayday.md.RESTART_INSTALL");
+            sendBroadcast(broadcastIntent);
+
+            finish();
+            return;
+        }
+
+        PBDatabase dbInstance = new PBDatabase(this);
+        dbInstance.open();
+        currentPage = dbInstance.retrievePage(pageId, selectedLang);
+        dbInstance.close();
+
+        if (currentPage == null) {
+            Log.e(">>>>>>", "page = null");
+            Toast.makeText(this, "Still to be implemented.", Toast.LENGTH_SHORT).show();
+            AppConstants.PAGE_FROM_NOT_IMPLEMENTED = true;
+            finish();
+            return;
+        } else {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+            Fragment fragment = null;
+
+            if (currentPage.getType().equals("simple")) {
+                tvToastMessage.setVisibility(View.INVISIBLE);
+                fragment = new SimpleFragment().newInstance(pageId, AppConstants.FROM_MAIN_ACTIVITY);
+            }else if (currentPage.getType().equals("warning")) {
+                    tvToastMessage.setVisibility(View.INVISIBLE);
+                    fragment = new WarningFragment().newInstance(pageId,AppConstants.FROM_MAIN_ACTIVITY);
+                
+            } else if (currentPage.getType().equals("modal")){
+                tvToastMessage.setVisibility(View.INVISIBLE);
+                Intent i = new Intent(MainActivity.this, MainModalActivity.class);
+                i.putExtra("page_id", pageId);
+//                i.putExtra("parent_activity", AppConstants.FROM_MAIN_ACTIVITY);
+                startActivity(i);
+                finish();
+                return;
+            } else {
+                if (currentPage.getComponent().equals("contacts"))
+                    fragment = new SetupContactsFragment().newInstance(pageId, AppConstants.FROM_MAIN_ACTIVITY);
+                else if (currentPage.getComponent().equals("message"))
+                    fragment = new SetupMessageFragment().newInstance(pageId, AppConstants.FROM_MAIN_ACTIVITY);
+                else if (currentPage.getComponent().equals("code"))
+                    fragment = new SetupCodeFragment().newInstance(pageId, AppConstants.FROM_MAIN_ACTIVITY);
+                else if (currentPage.getComponent().equals("alert"))
+                    fragment = new MainSetupAlertFragment().newInstance(pageId, AppConstants.FROM_MAIN_ACTIVITY);
+                else if (currentPage.getComponent().equals("language"))
+                    fragment = new LanguageSettingsFragment().newInstance(pageId, AppConstants.FROM_MAIN_ACTIVITY);
+                else
+                    fragment = new SimpleFragment().newInstance(pageId, AppConstants.FROM_MAIN_ACTIVITY);
+            }
+            fragmentTransaction.add(R.id.fragment_container, fragment);
+            fragmentTransaction.commit();
+        }
+    }
+
+
+    private void changeAppIcontoPB() {
+    	Log.e("MainActivity.changeAppIcontoPB", "");
+    	getPackageManager().setComponentEnabledSetting(
+                new ComponentName("io.mayday.md", "io.mayday.md.HomeActivity-setup"),
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
+/*
+        getPackageManager().setComponentEnabledSetting(
+                new ComponentName("io.mayday.md", "io.mayday.md.HomeActivity-calculator"),
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
+                */
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (ApplicationSettings.isAlertActive(MainActivity.this)) {
+            Log.e(">>>>>>", "HomeAlerting");
+            pageId = "home-alerting";
+            Intent i = new Intent(MainActivity.this, WizardActivity.class);
+            i.putExtra("page_id", pageId);
+            startActivity(i);
+        }
+        Log.e("MainActivity.onResume", "pageId = " + pageId + " and flagRiseFromPause = " + flagRiseFromPause);
+
+
+                /*
+        Check-1
+        if this page is resumed from the page still not implemented, then we'll handle it here.
+        If we don't do this check, then the resume procedure falls under Check-3 & execute that code snippet, which is not proper.
+         */
+        if (AppConstants.PAGE_FROM_NOT_IMPLEMENTED) {
+            Log.e("MainActivity.onResume", "returning from not-implemented page.");
+            AppConstants.PAGE_FROM_NOT_IMPLEMENTED = false;
+            return;
+        }
+
+        /*
+        Check-2
+        if this page is resumed by navigating-back from the next page, then we'll handle it here.
+        If we don't do this check, then the resume procedure falls under Check-3 & execute that code snippet, which is not proper.
+         */
+        if (AppConstants.IS_BACK_BUTTON_PRESSED) {
+            Log.e("MainActivity.onResume", "back button pressed");
+            AppConstants.IS_BACK_BUTTON_PRESSED = false;
+            return;
+        }
+
+        /*
+        if (flagRiseFromPause) {
+            Intent i = new Intent(MainActivity.this, CalculatorActivity.class);
+            startActivity(i);
+            overridePendingTransition(R.anim.show_from_bottom, R.anim.hide_to_top);
+
+            callFinishActivityReceiver();
+
+            finish();
+            return;
+        }
+        */
+
+        return;
+    }
+    
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e("MainActivity.onPause", ".");
+        Log.e("MainActivity.onPause", "flagRiseFromPause = " + true);
+        flagRiseFromPause = true;
+    }
+    
+    protected void onStop(){
+        super.onStop();
+        Log.e("MainActivity.onStop", ".");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+//        if(pageId.equals("home-not-configured")){
+//            Log.e("??????????????", "home-not-configured");
+//        	ApplicationSettings.setWizardState(MainActivity.this, AppConstants.WIZARD_FLAG_HOME_NOT_CONFIGURED);
+//        	Intent i = new Intent(MainActivity.this, WizardActivity.class);
+//        	i.putExtra("page_id", "home-not-configured");
+//        	startActivity(i);
+//        }
+        Log.e("MainActivity.onStart", ".");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(pageId.equals("home-ready")){
+            // don't go back
+//        	finish();
+//        	startActivity(AppUtil.behaveAsHomeButton());
+        }
+        else{
+            super.onBackPressed();
+            AppConstants.IS_BACK_BUTTON_PRESSED = true;
+        }
+    }
+
+
+
+
+
 }
--- mobile/src/main/java/io/mayday/md/trigger/BootReceiver.java
+++ mobile/src/main/java/io/mayday/md/trigger/BootReceiver.java
@@ -1,7 +1,35 @@
 package io.mayday.md.trigger;
 
-/**
- * Created by jshultz on 2/12/15.
- */
-public class BootReceiver {
+import io.mayday.md.alert.PanicAlert;
+import io.mayday.md.common.AppConstants;
+import io.mayday.md.common.ApplicationSettings;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+
+import static android.content.Intent.ACTION_BOOT_COMPLETED;
+
+public class BootReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if(intent.getAction().equals(ACTION_BOOT_COMPLETED)) {
+//        	new PanicAlert(context).deActivate();
+        	if(ApplicationSettings.isAlertActive(context)){
+                Log.e("BootReceiver", "Alarm is active");
+	        	ApplicationSettings.setAlertActive(context, false);
+	        	new PanicAlert(context).activate();
+        	}
+
+            int wizardState = ApplicationSettings.getWizardState(context.getApplicationContext());
+            Log.e("BootReceiver", "wizardState = " + wizardState);
+            if (wizardState == AppConstants.WIZARD_FLAG_HOME_READY) {
+                Log.e("BootReceiver", "BootReceiver in Panic Button");
+                context.startService(new Intent(context, HardwareTriggerService.class));
+            }
+        }
+    }
 }
